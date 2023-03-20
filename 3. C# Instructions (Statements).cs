cd03b365dee345b3b9257ea9b46a1f64de30c244
Vsevolod Rukavishnikov <107400712+SevaDactyl@users.noreply.github.com>
2023-03-20 23:47:57 +0500

    C#: Instructions (Statements)

---
+++ 3. C# Instructions (Statements).cs
@@ -0,0 +1,12 @@
+/*
+Выведите на экран друг за другом три имени: Robert, Stannis, Renly. В результате на экране должно отобразиться:
+
+Robert
+Stannis
+Renly
+Для каждого имени используйте свой собственный вызов Console.WriteLine().
+*/
+
+Console.WriteLine("Robert");
+Console.WriteLine("Stannis");
+Console.WriteLine("Renly");

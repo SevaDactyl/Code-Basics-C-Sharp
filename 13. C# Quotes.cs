fa6b893c8d6b8a4256f119c0e84909cc0ebf9890
Vsevolod Rukavishnikov <107400712+SevaDactyl@users.noreply.github.com>
2023-04-03 23:33:38 +0500

    C#: Quotes

---
+++ 13. C# Quotes.cs
@@ -0,0 +1,8 @@
+/*
+Напишите программу, которая выведет на экран:
+
+"Khal Drogo's favorite word is "athjahakar""
+Программа должна вывести на экран эту фразу в точности. Обратите внимание на кавычки в начале и в конце фразы.
+*/
+
+Console.WriteLine("\"Khal Drogo's favorite word is \"athjahakar\"\"");

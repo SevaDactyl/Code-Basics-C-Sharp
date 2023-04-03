908bb2ab6e273813e7ccbeb8039b24d58b1a3c0d
Vsevolod Rukavishnikov <107400712+SevaDactyl@users.noreply.github.com>
2023-04-03 23:26:27 +0500

    feat: delete

--- 43. JavaScript Methods.js
+++
@@ -1,9 +0,0 @@
-/*
-Приведите строку text к нижнему регистру и напечатайте её на экран.
-*/
-
-const text = 'a MIND needs Books as a Sword needS a WHETSTONE, if it is to keep its edge.';
-
-// BEGIN (write your solution here)
-console.log(text.toLowerCase());
-// END

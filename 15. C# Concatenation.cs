8b0a49867f317463d0fdbbd52f740ced67f51de9
Vsevolod Rukavishnikov <107400712+SevaDactyl@users.noreply.github.com>
2023-04-05 23:41:33 +0500

    C#: Concatenation

---
+++ 15. C# Concatenation.cs
@@ -0,0 +1,8 @@
+/*
+Используя Console.WriteLine(), выполните конкатенацию слов и выведите на экран фразу:
+
+Winter came for the House of Frey.
+Каждое слово должно быть записано отдельно и взято в кавычки, например 'Winter '. Не забудьте о пробелах после каждого слова.
+*/
+
+Console.WriteLine("Winter " + "came " + "for " + "the " + "House " + "of " + "Frey.");

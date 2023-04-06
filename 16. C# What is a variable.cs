9b6a296bc359d285889a2b1b8ba52fe38b58e7ff
Vsevolod Rukavishnikov <107400712+SevaDactyl@users.noreply.github.com>
2023-04-06 22:59:15 +0500

    C#: What is a variable

---
+++ 16. C# What is a variable.cs
@@ -0,0 +1,10 @@
+/*
+Создайте переменную с именем motto и содержимым What Is Dead May Never Die!. Распечатайте содержимое переменной.
+
+
+What Is Dead May Never Die!
+
+*/
+
+var motto = "What Is Dead May Never Die!";
+Console.WriteLine(motto);

2c82e0701c58967e1ae7ec4e386d443d96783da7
Vsevolod Rukavishnikov <107400712+SevaDactyl@users.noreply.github.com>
2023-04-11 23:44:57 +0500

    C#: Errors when working with variables

---
+++ 19. C# Errors when working with variables.cs
@@ -0,0 +1,11 @@
+/*
+Найдите в программе необъявленную переменную и объявите ее, присвоив ей значение "Dragon";
+*/
+
+var family = "Targaryen";
+
+// BEGIN (write your solution here)
+var pet = "Dragon";
+// END
+
+Console.WriteLine(family + " and " + pet);

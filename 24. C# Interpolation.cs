678037a6d0151c0ebcc6a143474447c679161905
Vsevolod Rukavishnikov <107400712+SevaDactyl@users.noreply.github.com>
2023-04-18 23:51:06 +0500

    C#: Interpolation

---
+++ 24. C# Interpolation.cs
@@ -0,0 +1,11 @@
+/*
+Выведите на экран строку Do you want to eat, <name>?, где вместо <name> должна использоваться константа stark. Вывод должен получиться таким:
+
+Do you want to eat, Arya?
+*/
+
+var stark = "Arya";
+
+// BEGIN (write your solution here)
+Console.WriteLine($"Do you want to eat, {stark}?");
+// END

c9ead40161447209b30b87458e237cfbd5e0edbe
Vsevolod Rukavishnikov <107400712+SevaDactyl@users.noreply.github.com>
2023-04-25 23:10:11 +0500

    C#: Null value

---
+++ 28. C# Null value.cs
@@ -0,0 +1,10 @@
+/*
+Определите переменную с именем greeting, но не инициализируйте ее
+*/
+
+// BEGIN (write your solution here)
+string greeting
+// END
+
+greeting = "Hello Hexlet!";
+Console.WriteLine(greeting);

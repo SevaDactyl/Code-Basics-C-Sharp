5e0ff42ea5146bc6b2816181e2a66340e8e8eb4e
Vsevolod Rukavishnikov <107400712+SevaDactyl@users.noreply.github.com>
2023-04-29 23:43:11 +0500

    C#: Properties

---
+++ 32. C# Properties.cs
@@ -0,0 +1,12 @@
+/*
+Создайте строку c именем "Robert I Baratheon" и, используя свойство Length этой строки, выведите на экран:
+
+
+Robert I Baratheon has 18 characters in his name
+
+*/
+
+var kingName = "Robert I Baratheon";
+var kingNameLength = kingName.Length;
+
+Console.WriteLine($"{kingName} has {kingNameLength} characters in his name");

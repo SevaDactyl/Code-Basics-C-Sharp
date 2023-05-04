68655dc71d007481bb38de9649af1610f549398d
Vsevolod Rukavishnikov <107400712+SevaDactyl@users.noreply.github.com>
2023-05-04 23:14:14 +0500

    C#: Side Effects

---
+++ 37. C# Side Effects.cs
@@ -0,0 +1,11 @@
+/*
+Напишем код в стиле "повтори за учителем". Попробуйте прочитать послание из файла message-from-stark.txt и вывести его содержимое на экран:
+
+// Читаем содержимое файла в переменную
+var message = File.ReadAllText("message-from-stark.txt");
+// Выводим содержимое на экран
+Console.WriteLine(message);
+*/
+
+var message = File.ReadAllText("message-from-stark.txt");
+Console.WriteLine(message);
